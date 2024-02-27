78d5505f9315d325b61e94833137110323df296e
mikegodfrinne <mike.godfrinne.dev@gmail.com>
2024-02-27 10:55:25 +0100

    Add and setup express-session and morgan

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const morgan = require('morgan');
 
 const app = express();
-const { NODE_ENV, PORT} = process.env;
+const { NODE_ENV, PORT, SECRET_SESSION} = process.env;
 const homeRouter = require('./routes/home.route.js');
 
 app.set('views', './views');
@@ -11,7 +13,16 @@ app.set('view engine', "ejs");
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('/public'));
 
+app.use(session({
+    secret: SECRET_SESSION,
+    resave: false,
+    saveUninitialized: true
+}));
+
+
 app.use(homeRouter);
+
+app.use(morgan('tiny'));
 
 // Database Instance
 require('./models');
